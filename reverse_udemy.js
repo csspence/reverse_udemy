ed88b1f87f92c43afe6c5eb09859a220e9311ed1
Crew Spence <csspence123@gmail.com>
2020-07-11 10:13:04 -0500

    Wrote working solution.

--- reverse_udemy.js
+++ reverse_udemy.js
@@ -5,8 +5,14 @@ reverse
 Write a recursive function called reversse which accepts a string and returns a new string in reverse.
 */
 
-function reverse(){
-  // add whatever parameters you deem necessary - good luck!
+const reverse = (str) => {
+  let newStr = str.slice(0, str.length - 1)
+  let revStr = str[str.length - 1];
+  if(str.length === 1) {
+    return str;
+  } else {
+    return revStr + reverse(newStr);
+  }
 }
 
 // reverse('awesome') // 'emosewa'
